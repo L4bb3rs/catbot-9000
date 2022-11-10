f08aed80561b1e21086c30032bfa5ce1b6680c97
King Leonidas <72262071+L4bb3rs@users.noreply.github.com>
2022-11-10 22:07:43 +0000

    Comment out of failing code
    Market Maker failing

--- catbot/server.js
+++ catbot/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { TokenController } = require('./controllers/TokenController');
 const { startDiscordBot } = require('./workers/discord/DiscordBot');
-const { startMarketMaker } = require('./workers/market-maker/MarketMakerBot');
+//const { startMarketMaker } = require('./workers/market-maker/MarketMakerBot');
 
 const refreshTokenCache = async () => {
 	const tc = new TokenController();
@@ -10,9 +10,9 @@ const refreshTokenCache = async () => {
 };
 
 const start = async () => {
-	await startMarketMaker();
+	//await startMarketMaker();
 	await refreshTokenCache();
-	setInterval(startMarketMaker, process.env.ORDER_CHECK_INTERVAL * 1000);
+	//setInterval(startMarketMaker, process.env.ORDER_CHECK_INTERVAL * 1000);
 	setInterval(refreshTokenCache, process.env.TOKEN_CACHE_LIFETIME * 1000);
 	await startDiscordBot();
 };
